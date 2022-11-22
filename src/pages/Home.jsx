1f012df7f1701dc01224ffbed27e9ec6dc4cd487
gabrielMatosBoubee <gm.boubee@gmail.com>
2022-11-22 18:08:26 -0300

    adicionei o filtro pela busca ao nome do planeta

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import starWarsApi from '../services';
 
 function Home() {
   const [planets, setPlanets] = useState([]);
+  const [planetsFilter, setPlanetsFilter] = useState([]);
+  const [name, setName] = useState('');
 
   const callApi = async () => {
     setPlanets(await starWarsApi());
@@ -11,41 +13,61 @@ function Home() {
     callApi();
   }, []);
 
+  useEffect(() => {
+    if (name.length === 0) {
+      setPlanetsFilter(planets);
+    }
+  }, [name.length, planets]);
+
+  const filterName = ({ target: { value } }) => {
+    setName(value);
+    if (value.length > 0) {
+      const planetsFilterByName = planets
+        .filter((element) => element.name.includes(value));
+      setPlanetsFilter(planetsFilterByName);
+    }
+  };
+
   return (
-    <table>
-      <tr>
-        <th>Name</th>
-        <th>Rotation Period</th>
-        <th>Orbital Period</th>
-        <th>Diameter</th>
-        <th>Climate</th>
-        <th>Gravity</th>
-        <th>Terrain</th>
-        <th>Surface Water</th>
-        <th>Population</th>
-        <th>Films</th>
-        <th>Created</th>
-        <th>Edited</th>
-        <th>URL</th>
-      </tr>
-      {planets.map((ele) => (
-        <tr key={ ele.name }>
-          <td>{ele.name}</td>
-          <td>{ele.rotation_period}</td>
-          <td>{ele.orbital_period}</td>
-          <td>{ele.diameter}</td>
-          <td>{ele.climate}</td>
-          <td>{ele.gravity}</td>
-          <td>{ele.terrain}</td>
-          <td>{ele.surface_water}</td>
-          <td>{ele.population}</td>
-          <td>{ele.films.map((element) => `${element} `)}</td>
-          <td>{ele.created}</td>
-          <td>{ele.edited}</td>
-          <td>{ele.url}</td>
+    <div>
+      <form>
+        <input type="text" onChange={ filterName } data-testid="name-filter" />
+      </form>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
         </tr>
-      ))}
-    </table>
+        {planetsFilter.map((ele) => (
+          <tr key={ ele.name }>
+            <td>{ele.name}</td>
+            <td>{ele.rotation_period}</td>
+            <td>{ele.orbital_period}</td>
+            <td>{ele.diameter}</td>
+            <td>{ele.climate}</td>
+            <td>{ele.gravity}</td>
+            <td>{ele.terrain}</td>
+            <td>{ele.surface_water}</td>
+            <td>{ele.population}</td>
+            <td>{ele.films.map((element) => `${element} `)}</td>
+            <td>{ele.created}</td>
+            <td>{ele.edited}</td>
+            <td>{ele.url}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
   );
 }
 
